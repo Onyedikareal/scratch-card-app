5029cc7eb9d490a4322a5bb0df16d11031d3c4ff
Onyedikareal <frankonyeka25@gmail.com>
2019-05-29 23:27:07 +0100

    create filltable function that populates html table

--- script/index.js
+++ script/index.js
@@ -51,24 +51,49 @@ function postCardData(url, data, myFunct) {
     xhttp.send(data);
   }
 
-  
+  function fillTable(cardObj){
+
+    
+    $('thead').append(`<tr>
+    <th >No</th>
+    <th >Card Pin</th>
+    <th >Serial</th>
+    <th >validity</th>
+    </tr>`);
+   
+
+    $('tbody').append("<tr id='card'></tr>");
+    
+    var td1 = `<th rowspan='2' scope='rowgroup' id='id' > ${cardObj.id}  </td>`;
+    var td2 =  `<td id="pin" >${cardObj.pin}</td>`;
+    var td3 = `<td id="sn">${cardObj.sn}</td>`
+    var td4 = `<td id = "validity">${cardObj.validity}</th>`
+    $('tbody #card').append(td1, td2, td3,td4);
+   
+    let td5 = `<td scope = "row">&nbsp;</td>`;
+    let td6 = `<td> <button class="btn bg-success" id="updatebutton">update</button></td>`;
+    let td7 =`<td><button class="btn bg-danger " id="trash">trash</button></td>`;
+    $('tbody').append("<tr id='update'></tr>")
+    $('tbody #update').append(td5,td6,td7);
+}
+
   
 $('#generate').click(function(){
-$('#table').removeClass('d-none');
-$('tbody').text('');
-$('thead').text('');
-var cardObj = cardObjGenerator();
- var data = JSON.stringify(cardObj);
-postCardData('http://localhost:3000/card', data, generateCard);
-var cardObject
-function generateCard(cardString){
-    cardObject = JSON.parse(cardString);
+    $('#table').removeClass('d-none');
+    $('tbody').text('');
+    $('thead').text('');
+    var cardObj = cardObjGenerator();
+    var data = JSON.stringify(cardObj);
+    postCardData('http://localhost:3000/card', data, generateCard);
+    var cardObject
+    function generateCard(cardString){
+        cardObject = JSON.parse(cardString);
 
-    fillTable(cardObject);
-    var cardDate = cardObject.validity.toString();
-    validityCheck(cardDate);
+        fillTable(cardObject);
+        var cardDate = cardObject.validity.toString();
+        validityCheck(cardDate);
 
-}
+    }
 
 });
 
