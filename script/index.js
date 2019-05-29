15bbc6d7509cb18fdc449cc77747eb0760c55057
Onyedikareal <frankonyeka25@gmail.com>
2019-05-29 13:27:16 +0100

    create random date generator functin

--- script/index.js
+++ script/index.js
@@ -12,8 +12,17 @@ function serialGenerator(){
 }
 
 function dayMonthGenerator(max,min){
-var random = Math.floor(Math.random() * (+max - +min) + +max); 
+var random = Math.floor(Math.random() * (max - min) + min); 
 return random;
+}
+
+function dateGenerator(){
+    var month = dayMonthGenerator(12, 0);
+    var dayOfMonth = dayMonthGenerator(31,0);
+    var d  = new Date(2019,month, dayOfMonth);
+    console.log(month);
+    console.log(dayOfMonth)
+    return d;
 }
 
 
