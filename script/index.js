c6528fa9dabf4317eaeaf26f6391ba3ab1e927ee
Onyedikareal <frankonyeka25@gmail.com>
2019-05-30 23:37:55 +0100

    create function to trigger bootstrap modal form

--- script/index.js
+++ script/index.js
@@ -201,4 +201,18 @@ $('#tbody').on('click', '#trash',  function(){
     }
 })
 
-  
+var button;
+$('#exampleModal').on('show.bs.modal', function (event) {
+    button = $(event.relatedTarget) // Button that triggered the modal
+    
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    //modal.find('.modal-title').text()
+    modal.find('#serialmodal').val(recipient)
+  })
+
+ 
+
+ 
